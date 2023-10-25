a3d603422022afe4c8aad6a0e6b7331f0f136977
mohammedafsalk <techafs456@gmail.com>
2023-10-25 16:32:44 +0530

    changes made

--- app.js
+++ app.js
@@ -14,18 +14,19 @@ const app = express();
 app.engine("hbs", engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 
-Handlebars.registerHelper('ifEqual', function(a, b, options) {
-    if (a === b) {
-      return options.fn(this);
-    } else {
-      return options.inverse(this);
-    }
-  });
+Handlebars.registerHelper("ifEqual", function (a, b, options) {
+  if (a === b) {
+    return options.fn(this);
+  } else {
+    return options.inverse(this);
+  }
+});
 
 app.use(
   session({
     store: MongoStore.create({
-      mongoUrl: "mongodb://127.0.0.1:27017/E_commerce",
+      mongoUrl:
+        "mongodb+srv://techafs456:GOGGOQKRiDh9Cpl9@cluster1.q8lap20.mongodb.net/?retryWrites=true&w=majority",
     }),
     secret: "12656",
     resave: false,
@@ -37,10 +38,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 
 app.use(function (req, res, next) {
-  res.header(
-    "Cache-Control",
-    "no-cache, private, no-store, must-revalidate"
-  );
+  res.header("Cache-Control", "no-cache, private, no-store, must-revalidate");
   next();
 });
 
@@ -54,4 +52,3 @@ app.use(function (req, res, next) {
 app.listen(4000, () => {
   console.log("server running");
 });
-
