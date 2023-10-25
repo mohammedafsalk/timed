f0b70b5e7e3017b86a10cb1900a9bb841f736716
mohammedafsalk <techafs456@gmail.com>
2023-10-25 15:56:30 +0530

    atlas added

--- dbConnect.js
+++ dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const dbConnect = ()=>{
     mongoose.set('strictQuery',true)
-    mongoose.connect('mongodb://127.0.0.1:27017/E_commerce')
+    mongoose.connect(process.env.MONGOPASS)
     .then(()=>{
         console.log(' DB connected');  
         
