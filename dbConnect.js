ca071628c773bcc4001926202bb21522fb1303a0
mohammedafsalk <techafs456@gmail.com>
2023-10-25 16:14:21 +0530

    changes made

--- dbConnect.js
+++ dbConnect.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose')
 
-const dbConnect = ()=>{
-    mongoose.set('strictQuery',false)
-    mongoose.connect(process.env.MONGOPASS)
-    .then(()=>{
-        console.log(' DB connected');  
-        
+const mongoose = require("mongoose");
+const dbConnect = () => {
+  mongoose.set("strictQuery", false);
+  mongoose
+    .connect(`mongodb+srv://techafs456:GOGGOQKRiDh9Cpl9@cluster1.q8lap20.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => {
+      console.log(" DB connected");
     })
-    .catch(err=>{
-        console.log('error'+err);
-    })
-}
+    .catch((err) => {
+      console.log("error" + err);
+    });
+};
 
-module.exports = dbConnect
+module.exports = dbConnect;
